9a660a7b7eba5304447ea3879bd908d6263023bc
Alfred DU <ihearmywords@hotmail.com>
2025-04-17 14:27:24 +0800

    resolve single entry file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Resolve imports of a python file or module, exclude site packages & builtin modules.
+
---
+++ src/phy_imports_resolver/__init__.py
@@ -0,0 +1,11 @@
+""" Resolve imports of a python file or module, exclude site 
+packages & builtin modules. 
+"""
+__version__ = "0.1.0"
+__all__ = [
+    'resolve_entry_file',
+]
+
+
+# imports
+from ._core import resolve_entry_file
