9a30237cbaf5dfbb9771bbec3da3f28507f59c30
Alfred DU <ihearmywords@hotmail.com>
2025-11-21 13:45:39 +0800

    keep consistent version range with other phy project

--- src/phy_imports_resolver/__init__.py
+++ src/phy_imports_resolver/__init__.py
@@ -1,5 +1,5 @@
 """ Resolve imports of a python file or module, exclude site packages & builtin modules. """
-__version__ = "0.3.1"
+__version__ = "0.1.2"
 __all__ = [
     'resolve',
     'FileModuleImportsNode',
